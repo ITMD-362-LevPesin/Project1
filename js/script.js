57c159bd97f31f6fe7795e9c7dc0914214626498
LevPesin-Tech <78404922+LevPesin-Tech@users.noreply.github.com>
2021-11-02 23:11:17 -0500

    fill in js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var js_avaliable = document.getElementsByTagName('html')[0];
+var form = document.getElementById("sign_form");
+
+function myFunction() {
+	const username = document.getElementById("username").value;
+	let message = "Thank you, " + username ". You will receive the newsletter soon. Up the toffees!";
+	alert(message);
+}
+
+form.addEventListener("submit", clickbutton);
